33cfa35e7d1ee51dca022e9d2d2e07b2718d892f
ashishgupta <ashish189000@gmail.com>
2025-12-19 23:32:54 +0530

    added career page and css file

---
+++ pages/career.tsx
@@ -0,0 +1,32 @@
+import ContactCode from '@/components/ContactCode';
+
+import styles from '@/styles/CareerPage.module.css';
+import { experience } from '@/data/experience';
+
+const CareerPage = () => {
+  return (
+    <div className={styles.layout}>
+      <h1 className={styles.pageTitle}>Career Ladder</h1>
+      <p className={styles.pageSubtitle}>
+        Feel free to reach out to me through any of the social platforms below.
+        I&apos;m always open to new opportunities and connections.
+      </p>
+      {experience.map((experience) => (
+        <div className={styles.container}>
+          <div className={styles.contactContainer}>
+            <ContactCode data={experience} />
+          </div>
+      </div>
+      ))}
+    </div>
+  );
+};
+
+export async function getStaticProps() {
+  return {
+    props: { title: 'Experience' },
+  };
+}
+
+export default CareerPage;
+
