9bb1c36a423911fa9aecd5e7aff1b7dcc58d5bdd
ashishgupta <ashish189000@gmail.com>
2025-12-19 23:31:25 +0530

    created data file for experiences

---
+++ data/experience.ts
@@ -0,0 +1,34 @@
+export interface Experience {
+  company: string;
+  role: string;
+  date: string;
+  description: string;
+}
+
+export const experience: Experience[] = [
+  {
+    company: 'Smartshift Technologies',
+    role: 'Software Developer - SAP ABAP',
+    date: 'July 2025 - Present',
+    description: 'Working with the team to develop and cater to SAP clients with their SAP ERP system.',
+  },
+  {
+    company: 'Smartshift Technologies',
+    role: 'Software Developer - Intern',
+    date: 'Jan 2025 - June 2025',
+    description: 'Working with the team to develop and cater to SAP clients with their SAP ERP system.',
+  },
+
+  {
+    company: 'PredictRAM',
+    role: 'Fullstack Developer - Flutter',
+    date: 'Jan 2024 - March 2024',
+    description: 'Working with the team to develop and cater to SAP clients with their SAP ERP system.',
+  },
+  {
+    company: 'Bennett University',
+    role: 'B.Tech in Computer Science Engineering',
+    date: 'Sept 2021 - Sept 2025',
+    description: 'Working with the team to develop and cater to SAP clients with their SAP ERP system.',
+  },
+];
