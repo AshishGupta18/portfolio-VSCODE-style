7db901fbce8ed1ee10f6c10d1884522078a9e73c
ashishgupta <ashish189000@gmail.com>
2026-01-15 18:20:04 +0530

    added speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Layout from '@/components/Layout';
 import Head from '@/components/Head';
 
@@ -20,6 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head title={`Ashish Gupta | ${pageProps.title}`} />
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights/>
     </Layout>
   );
 }
